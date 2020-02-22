5484d57734a020e1c8087bb19f3e659454b04de4
OFRIN <josanghyeokn@gmail.com>
2020-02-22 20:37:47 +0900

    update_20200222

---
+++ commit.py
@@ -0,0 +1,12 @@
+import os
+import time
+
+# output = print
+output = os.system
+
+data = time.localtime()
+
+output('git add .')
+output('git commit -m \"update_{}{:02d}{}\"'.format(data.tm_year, data.tm_mon, data.tm_mday))
+output('git push')
+
